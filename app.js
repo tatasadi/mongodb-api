005f4659ba293f79b5004ca749ac095031ab8b87
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-07-15 11:23:56 +0200

    update and remove item

--- app.js
+++ app.js
@@ -16,21 +16,26 @@ async function main() {
     //const results = await circulationRepo.loadData(data)
     //console.log(results.insertedCount, results.ops)
 
+    //*************** get data ***************//
     const getData = await circulationRepo.get()
-    assert.equal(data.length, getData.length)
+    //assert.equal(data.length, getData.length)
 
+    //*************** filter data ***************//
     const filterData = await circulationRepo.get({
       Newspaper: getData[4].Newspaper,
     })
     assert.deepEqual(filterData[0], getData[4])
 
+    //*************** limit data ***************//
     const limitData = await circulationRepo.get({}, 3)
     assert.equal(limitData.length, 3)
 
+    //*************** get by id ***************//
     const id = getData[4]._id.toString()
     const byId = await circulationRepo.getById(id)
     assert.deepEqual(byId, getData[4])
 
+    //*************** add new item ***************//
     const newItem = {
       Newspaper: "My Paper",
       "Daily Circulation, 2004": 1,
@@ -40,10 +45,28 @@ async function main() {
       "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
       "Pulitzer Prize Winners and Finalists, 1990-2014": 0,
     }
-    const addItem = await circulationRepo.add(newItem)
-    assert(addItem._id)
-    const addedItemQuery = await circulationRepo.getById(addedItem._id)
+    const addItemId = await circulationRepo.add(newItem)
+    assert(addItemId)
+    const addedItemQuery = await circulationRepo.getById(addItemId)
     assert.deepEqual(addedItemQuery, newItem)
+
+    //*************** update existing item ***************//
+    const updatedItem = await circulationRepo.update(addItemId, {
+      Newspaper: "My new paper",
+      "Daily Circulation, 2004": 1,
+      "Daily Circulation, 2013": 2,
+      "Change in Daily Circulation, 2004-2013": 100,
+      "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+      "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
+      "Pulitzer Prize Winners and Finalists, 1990-2014": 0,
+    })
+    assert.equal(updatedItem.Newspaper, "My new paper")
+
+    //*************** remove existing item ***************//
+    const removed = await circulationRepo.remove(addItemId)
+    assert(removed)
+    const deletedItem = await circulationRepo.getById(addItemId)
+    assert.equal(deletedItem, null)
   } catch (error) {
     console.log(error)
   } finally {
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -59,7 +59,7 @@ function circulationRepo() {
         await client.connect()
         const db = client.db(process.env.MONGODB_DB_NAME)
         const addedItem = await db.collection("newspapers").insertOne(item)
-        resolve(addedItem.ops[0])
+        resolve(addedItem.insertedId)
         client.close()
       } catch (error) {
         reject(error)
@@ -67,7 +67,43 @@ function circulationRepo() {
     })
   }
 
-  return { loadData, get, getById, add }
+  function update(id, newItem) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(process.env.MONGODB_URL)
+      try {
+        await client.connect()
+        const db = client.db(process.env.MONGODB_DB_NAME)
+        const updatedItem = await db
+          .collection("newspapers")
+          .findOneAndReplace({ _id: new ObjectId(id) }, newItem, {
+            returnDocument: "after",
+          })
+        resolve(updatedItem.value)
+        client.close()
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  function remove(id) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(process.env.MONGODB_URL)
+      try {
+        await client.connect()
+        const db = client.db(process.env.MONGODB_DB_NAME)
+        const removed = await db
+          .collection("newspapers")
+          .deleteOne({ _id: new ObjectId(id) })
+        resolve(removed.deletedCount === 1)
+        client.close()
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  return { loadData, get, getById, add, update, remove }
 }
 
 module.exports = circulationRepo()
