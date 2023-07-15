a5aa495f0939879afc44c0a1b908cd04edaf5c5e
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-07-15 10:57:47 +0200

    get - getbyid - add

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const dotenv = require("dotenv")
-const MongoClient = require("mongodb").MongoClient
+const { MongoClient } = require("mongodb")
 const circulationRepo = require("./repos/circulationRepo")
 const data = require("./circulation.json")
+const assert = require("assert")
 
 dotenv.config()
 const url = process.env.MONGODB_URL
@@ -15,16 +16,42 @@ async function main() {
     //const results = await circulationRepo.loadData(data)
     //console.log(results.insertedCount, results.ops)
 
-    const items = await circulationRepo.get()
-    console.log(items)
+    const getData = await circulationRepo.get()
+    assert.equal(data.length, getData.length)
+
+    const filterData = await circulationRepo.get({
+      Newspaper: getData[4].Newspaper,
+    })
+    assert.deepEqual(filterData[0], getData[4])
+
+    const limitData = await circulationRepo.get({}, 3)
+    assert.equal(limitData.length, 3)
+
+    const id = getData[4]._id.toString()
+    const byId = await circulationRepo.getById(id)
+    assert.deepEqual(byId, getData[4])
+
+    const newItem = {
+      Newspaper: "My Paper",
+      "Daily Circulation, 2004": 1,
+      "Daily Circulation, 2013": 2,
+      "Change in Daily Circulation, 2004-2013": 100,
+      "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+      "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
+      "Pulitzer Prize Winners and Finalists, 1990-2014": 0,
+    }
+    const addItem = await circulationRepo.add(newItem)
+    assert(addItem._id)
+    const addedItemQuery = await circulationRepo.getById(addedItem._id)
+    assert.deepEqual(addedItemQuery, newItem)
   } catch (error) {
     console.log(error)
   } finally {
-    const admin = client.db(dbName).admin()
+    //const admin = client.db(dbName).admin()
     //console.log(await admin.serverStatus())
 
     //await client.db(dbName).dropDatabase()
-    console.log(await admin.listDatabases())
+    //console.log(await admin.listDatabases())
 
     client.close()
   }
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -1,4 +1,4 @@
-const { MongoCLient, MongoClient } = require("mongodb")
+const { MongoClient, ObjectId } = require("mongodb")
 const dotenv = require("dotenv")
 
 function circulationRepo() {
@@ -8,10 +8,8 @@ function circulationRepo() {
       try {
         await client.connect()
         const db = client.db(process.env.MONGODB_DB_NAME)
-
         results = await db.collection("newspapers").insertMany(data)
         resolve(results)
-
         client.close()
       } catch (error) {
         reject(error)
@@ -19,16 +17,17 @@ function circulationRepo() {
     })
   }
 
-  function get() {
+  function get(query, limit) {
     return new Promise(async (resolve, reject) => {
       const client = new MongoClient(process.env.MONGODB_URL)
       try {
         await client.connect()
         const db = client.db(process.env.MONGODB_DB_NAME)
-
-        const items = db.collection("newspapers").find()
+        let items = db.collection("newspapers").find(query)
+        if (limit > 0) {
+          items = items.limit(limit)
+        }
         resolve(await items.toArray())
-
         client.close()
       } catch (error) {
         reject(error)
@@ -36,7 +35,39 @@ function circulationRepo() {
     })
   }
 
-  return { loadData, get }
+  function getById(id) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(process.env.MONGODB_URL)
+      try {
+        await client.connect()
+        const db = client.db(process.env.MONGODB_DB_NAME)
+        const item = await db
+          .collection("newspapers")
+          .findOne({ _id: new ObjectId(id) })
+        resolve(item)
+        client.close()
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  function add(item) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(process.env.MONGODB_URL)
+      try {
+        await client.connect()
+        const db = client.db(process.env.MONGODB_DB_NAME)
+        const addedItem = await db.collection("newspapers").insertOne(item)
+        resolve(addedItem.ops[0])
+        client.close()
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  return { loadData, get, getById, add }
 }
 
 module.exports = circulationRepo()
